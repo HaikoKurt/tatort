c3449f464a23e4fbbc28bb4e26d708989fa984ea
Haiko Emmel <mail@haiko-emmel.de>
2022-03-17 18:30:10 +0100

    Kommentar entfernt und print aufgehübscht.

--- tatort_file_map.py
+++ tatort_file_map.py
@@ -13,16 +13,8 @@ def move(source, dest) :
     except :
         pass
     os.rename(source, dest)
+    print(f"{source} -> {dest}")
 
-# regular expression to identify a file that is named with episode numbers from the total ordering
-# capture group 1: episode number (total order)
-# capture group 2: file name (excluding the extension ".mp4")
-# E.g., "0586 Dunkle Wege.mp4":
-#   capture group 1: "0586"
-#   capture grpup 2: "Dunkle Wege"
-# Episide numbers from https://de.wikipedia.org/wiki/Liste_der_Tatort-Folgen
-# Also, note that all episode numbers are ALWAYS 4 digits (with leading zeros if required). This last
-# requirement can easily be relaxed with a slightly different regex 
 renamer = Renamer()
 
 for f in Path(SOURCE_DIR).iterdir():
@@ -32,13 +24,11 @@ for f in Path(SOURCE_DIR).iterdir():
             source = f"{f.parent}/{f.name}"
             dest = f"{DEST_DIR}/{new_name}"
             duplicate = f"{DUPLICATE_DIR}/{f.name}"
-            # os.rename() actually renames and moves the file. If the destination already exists, it will be replaced
             if not os.path.isfile(dest) :
+                print(f">>> {f.name} moved")
                 move(source, dest)
             else :
                 print(f"*** {f.name} duplicate")
                 move(source, duplicate)
-            # print the partial file name to the console for confirmation
-            print(f"{source} -> {dest}")
         else :
-            print(f"*** {f.name} not found")
+            print(f"??? {f.name} not found")
