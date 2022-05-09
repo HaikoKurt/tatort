9fac8f6c04f98b05d18a1f09224597be73707834
Haiko Emmel <mail@haiko-emmel.de>
2022-05-09 17:33:27 +0200

    Bibliothek watchdog durch eigenes Modul watchfolder ersetzt.

---
+++ renamer/src/watchfolder.py
@@ -0,0 +1,56 @@
+from pathlib import Path
+from time import sleep
+import _thread, os, logging
+
+class Watchfolder :
+    logger = logging.getLogger(__name__)
+
+    def __init__(self, folder, action = None, no_system_files = True, watch_size_period = 10, watch_dir_period = 1) -> None:
+        self.folder = folder
+        self.no_system_files = no_system_files
+        self.watch_size_period = watch_size_period
+        self.watch_dir_period = watch_dir_period
+        self.under_observation = []
+        self.action = action
+
+    def __wait_and_execute(self, f) :
+        self.logger.info(f"waiting for {f.name}")
+        source = f"{f.parent}/{f.name}"
+        size = 0
+        while size < os.path.getsize(source) :
+            size = os.path.getsize(source)
+            self.logger.debug(f"{f.name} size: {size}")
+            sleep(self.watch_size_period)
+        self.logger.debug(f"{f.name} ready...")
+        try :
+            if self.action is None :
+                f.unlink(missing_ok=True)
+            else :
+                self.action(f)
+        except Exception as e:
+            self.logger.error(f"Exception occured: {e}")
+        finally :
+            self.under_observation.remove(f.name)
+
+    def __test_and_start(self, f) :
+        if f.is_file() and (not self.no_system_files or not f.name.startswith('.')) :
+            if not f.name in self.under_observation :
+                self.under_observation.append(f.name)
+                _thread.start_new_thread(self.__wait_and_execute, (f, ))
+
+    def watch(self) :
+        while True :
+            for f in Path(self.folder).iterdir():
+                self.__test_and_start(f)
+            sleep(self.watch_dir_period)
+
+if __name__ == "__main__" :
+    def move(f) :
+        print(f"Moving: {f.name}")
+        f.rename(f"/Users/haiko/Documents/Temp/destination/{f.name}")
+
+    wf = Watchfolder("/Users/haiko/Documents/Temp/watchfolder", action = move)
+    try :
+        wf.watch()
+    except KeyboardInterrupt :
+        pass
--- renamer/src/watchit.py
+++ renamer/src/watchit.py
@@ -1,12 +1,7 @@
-import time, os
-from datetime import datetime
-from watchdog.observers import Observer
-from watchdog.events import PatternMatchingEventHandler
-from renamer import Renamer
-from pathlib import Path
-import shutil, logging
+import os, json, shutil, logging
 from logging.config import dictConfig
-import json
+from renamer import Renamer
+from watchfolder import Watchfolder
 
 with open('logging.json', 'rt') as file :
     dictConfig(json.load(file))
@@ -27,54 +22,27 @@ def move(source, dest) :
     shutil.move(source, dest)
     logger.info(f"{source} -> {dest}")
 
-def on_modified(event):
-    try :
-        f = Path(event.src_path)
-        source = f"{f.parent}/{f.name}"
-        start_size = 0
-        if f.is_file() :
-            start_size = os.path.getsize(source)
-        now = datetime.now()
-        if not f.name.startswith(".") and f.is_file() and start_size > 0 :
-            logger.info(f"{now} - {source}; size = {start_size}")
-            time.sleep(10)
-            size = os.path.getsize(source)
-            logger.info(f"{now} - {source}; size = {size}")
-            if not size > start_size :
-                logger.info(f"{now} - {source}, not growing ")
-                new_name = renamer.rename(f.name)
-                if new_name is not None :
-                    dest = f"{DEST_DIR}/{new_name}"
-                    duplicate = f"{DUPLICATE_DIR}/{f.name}"
-                    if not os.path.isfile(dest) :
-                        logger.info(f">>> {f.name} moved")
-                        move(source, dest)
-                    else :
-                        logger.info(f"*** {f.name} duplicate")
-                        move(source, duplicate)
-                else :
-                    logger.info(f"??? {f.name} not found")
-                    unknown = f"{UNKNOWN_DIR}/{f.name}"
-                    move(source, unknown)
-    except Exception as e :
-        logger.error(f"!!! Exception {e} occured")
+def file_arrived(f):
+    source = f"{f.parent}/{f.name}"
+    new_name = renamer.rename(f.name)
+    if new_name is not None :
+        dest = f"{DEST_DIR}/{new_name}"
+        duplicate = f"{DUPLICATE_DIR}/{f.name}"
+        if not os.path.isfile(dest) :
+            logger.info(f">>> {f.name} moved")
+            move(source, dest)
+        else :
+            logger.info(f"*** {f.name} duplicate")
+            move(source, duplicate)
+    else :
+        logger.info(f"??? {f.name} not found")
+        unknown = f"{UNKNOWN_DIR}/{f.name}"
+        move(source, unknown)
 
 if __name__ == "__main__":
-    patterns = ["*"]
-    ignore_patterns = None
-    ignore_directories = True 
-    case_sensitive = True
-    my_event_handler = PatternMatchingEventHandler(patterns, ignore_patterns, ignore_directories, case_sensitive)
-    my_event_handler.on_modified = on_modified
-
-    my_observer = Observer()
-    my_observer.schedule(my_event_handler, SOURCE_DIR, recursive=False)
-
     logger.info("Start watching...")
-    my_observer.start()
-    try:
-        while True:
-            time.sleep(1)
-    except KeyboardInterrupt:
-        my_observer.stop()
-        my_observer.join()
+    wf = Watchfolder(SOURCE_DIR, action = file_arrived)
+    try :
+        wf.watch()
+    except KeyboardInterrupt :
+        pass
