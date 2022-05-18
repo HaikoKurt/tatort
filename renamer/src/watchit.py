a7268fe6d95291038be716d9c1955a6bbfe55b65
Haiko Emmel <mail@haiko-emmel.de>
2022-05-18 10:21:55 +0200

    Benötigte Verzeichnisse beim start erzeugen.

--- renamer/src/watchit.py
+++ renamer/src/watchit.py
@@ -7,9 +7,11 @@ with open('logging.json', 'rt') as file :
     dictConfig(json.load(file))
 
 SOURCE_DIR = "/source"
+DUPLICATE_SUB_DIR = "duplicate"
+UNKNOWN_SUB_DIR = "unknown"
+LOG_SUB_DIR = "logs"
+
 DEST_DIR = "/destination"
-DUPLICATE_DIR = "/source/duplicate"
-UNKNOWN_DIR = "/source/unknown"
 
 renamer = Renamer()
 logger = logging.getLogger(__name__)
@@ -24,7 +26,7 @@ def file_arrived(f):
     new_name = renamer.rename(f.name)
     if new_name is not None :
         dest = f"{DEST_DIR}/{new_name}"
-        duplicate = f"{DUPLICATE_DIR}/{f.name}"
+        duplicate = f"{SOURCE_DIR}/{DUPLICATE_SUB_DIR}/{f.name}"
         if not os.path.isfile(dest) :
             logger.info(f">>> {f.name} moved")
             move(source, dest)
@@ -33,11 +35,16 @@ def file_arrived(f):
             move(source, duplicate)
     else :
         logger.info(f"??? {f.name} not found")
-        unknown = f"{UNKNOWN_DIR}/{f.name}"
+        unknown = f"{SOURCE_DIR}/{UNKNOWN_SUB_DIR}/{f.name}"
         move(source, unknown)
 
 if __name__ == "__main__":
+    os.makedirs(f"{SOURCE_DIR}/{DUPLICATE_SUB_DIR}", exist_ok=True)
+    os.makedirs(f"{SOURCE_DIR}/{UNKNOWN_SUB_DIR}", exist_ok=True)
+    os.makedirs(f"{SOURCE_DIR}/{LOG_SUB_DIR}", exist_ok=True)
+    
     logger.info("Start watching...")
+
     wf = Watchfolder(SOURCE_DIR, action = file_arrived)
     try :
         wf.watch()
